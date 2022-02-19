cca38fe2ba107dc4cca9423b0efab136d74959bd
Kenton Wang <smartepsh@gmail.com>
2022-02-20 01:49:00 +0800

    feat: prepare to publish

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule RuntimeProvider.MixProject do
   def project do
     [
       app: :runtime_provider,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      name: "RuntimeProvider",
+      source_url: github_url()
     ]
   end
 
@@ -24,9 +28,19 @@ defmodule RuntimeProvider.MixProject do
       {:yaml_elixir, "~> 2.5"},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
-
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"Github" => github_url()}
+    ]
+  end
+
+  defp description, do: "Make ConfigProvider easier to use with validation."
+
+  defp github_url do
+    "https://github.com/smartepsh/runtime-provider.git"
   end
 end
